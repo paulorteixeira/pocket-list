c8c3006b9279db4ca3b3f5e9088b733c12ece4a7
Paulo Ricardo <1981928paulo@gmail.com>
2022-08-04 15:29:24 -0300

    3af44ce

---
+++ js/about.8bb96489.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpocket_list"]=self["webpackChunkpocket_list"]||[]).push([[443],{1272:function(t,n,s){s.r(n),s.d(n,{default:function(){return c}});var u=function(){var t=this;t._self._c;return t._m(0)},e=[function(){var t=this,n=t._self._c;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],l=s(1001),i={},a=(0,l.Z)(i,u,e,!1,null,null,null),c=a.exports}}]);
+//# sourceMappingURL=about.8bb96489.js.map
